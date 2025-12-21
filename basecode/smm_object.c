1cad1034f87077cc2ca279a473f698a2106ae7e8
이성은 <katrina060208@gmail.com>
2025-12-21 20:42:08 +0900

    1221 수정6

--- basecode/smm_object.c
+++ basecode/smm_object.c
@@ -10,77 +10,40 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_NODENR											100
-#define MAX_NODETYPE   									7
-#define MAX_GRADE      									13
-#define MAX_ACHIEVABLE_GRADE 						9
+#define MAX_NODENR 100
+#define MAX_NODETYPE 7
+#define MAX_GRADE 13
+#define MAX_ACHIEVABLE_GRADE 9
 
-
-//구조체로 만들기 
-//struct int smm_players[MAX_PLAYER];
-
-// 각 플레이어가 수강한 강의 이력 노드를 나타내는 구조체
-typedef struct smmGradeNode {
-    char lecture_name[MAX_CHARNAME];
-    int credit;
-    smmGrade_e grade;
-    struct smmGradeNode *next;
-} smmGradeNode_t;
-	
 typedef struct {
-	int player_pos[MAX_PLAYER];
-	int player_credit[MAX_PLAYER];
-	char player_name[MAX_PLAYER][MAX_CHARNAME];
-	int player_energy[MAX_PLAYER];
-	int flag_graduated[MAX_PLAYER];
-	int is_experimenting[MAX_PLAYER]; 
-  int exp_success_value[MAX_PLAYER];
-	smmGradeNode_t *grade_history_head[MAX_PLAYER]; // pointer
+    int player_pos[MAX_PLAYER];
+    int player_credit[MAX_PLAYER];
+    char player_name[MAX_PLAYER][MAX_CHARNAME];
+    int player_energy[MAX_PLAYER];
+    int flag_graduated[MAX_PLAYER];
+    int is_experimenting[MAX_PLAYER]; 
+    int exp_success_value[MAX_PLAYER];
+    smmObj_t *grade_history_head[MAX_PLAYER];
 } smm_player_t;
 
-static 	smm_player_t smm_players;
+static smm_player_t smm_players;
 static int smm_PlayerNr = 0;
 static int smm_nodeNr = 0;
+static smmObj_t *smmObj_board[MAX_NODENR]; 
 
-static char smmNodeName[MAX_NODETYPE][MAX_CHARNAME] = { // array size [7][200]
-						"lecture",
-						"restaurant",
-						"laboratory",
-						"home",
-						"gotlab",
-						"foodchange",
-						"festival"
+static char smmNodeName[MAX_NODETYPE][MAX_CHARNAME] = {
+    "lecture", "restaurant", "laboratory", "home", "gotlab", "foodchange", "festival"
 };
 
-static char smmGradeName[MAX_GRADE][MAX_CHARNAME] = { // array size [7][200]
-						"A+",
-						"A0",
-						"A-",
-						"B+",
-						"B0",
-						"B-",
-						"C+",
-						"C0",
-						"C-",
-						"D+",
-						"D0",
-						"D-",
-						"F"
+static char smmGradeName[MAX_GRADE][MAX_CHARNAME] = {
+    "A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D+", "D0", "D-", "F"
 };
 
-static smmObj_object_t *smmObj_board[MAX_NODENR];
+// Player
+void smmObj_updatePlayerNr(int n) { smm_PlayerNr = n; }
+int smmObj_getPlayerNr(void) { return smm_PlayerNr; }
 
-// 1. PlayerNr setting and return
-void smmObj_updatePlayerNr(int n) {
-	smm_PlayerNr = n;
-	}
-	
-int smmObj_getPlayerNr(void) {
-	return smm_PlayerNr;
-	}
-
-void smmObj_initPlayerFields(int player, int initEnergy)
-{
+void smmObj_initPlayerFields(int player, int initEnergy) {
     smm_players.player_pos[player] = 0;
     smm_players.player_credit[player] = 0;
     smm_players.player_energy[player] = initEnergy;
@@ -90,183 +53,72 @@ void smmObj_initPlayerFields(int player, int initEnergy)
     smm_players.exp_success_value[player] = 0;
 }
 
-// 2. Initialization and update function
-void smmObj_updatePlayerPos(int player, int pos) {
-    smm_players.player_pos[player] = pos;
-}
+void smmObj_updatePlayerPos(int player, int pos) { smm_players.player_pos[player] = pos; }
+void smmObj_updatePlayerCredit(int player, int credit) { smm_players.player_credit[player] += credit; }
+void smmObj_updatePlayerEnergy(int player, int energy) { smm_players.player_energy[player] += energy; }
+void smmObj_updateGraduatedFlag(int player, int flag) { smm_players.flag_graduated[player] = flag; }
+char* smmObj_getPlayerName(int player) { return smm_players.player_name[player]; }
+void smmObj_setPlayerName(int player, char* name) { strcpy(smm_players.player_name[player], name); }
+int smmObj_getPlayerPos(int player) { return smm_players.player_pos[player]; }
+int smmObj_getPlayerCredit(int player) { return smm_players.player_credit[player]; }
+int smmObj_getPlayerEnergy(int player) { return smm_players.player_energy[player]; }
+int smmObj_getGraduatedFlag(int player) { return smm_players.flag_graduated[player]; }
 
-void smmObj_updatePlayerCredit(int player, int credit) {
-    smm_players.player_credit[player] += credit;
-}
-
-void smmObj_updatePlayerEnergy(int player, int energy) {
-    smm_players.player_energy[player] += energy;
-}
-
-void smmObj_updateGraduatedFlag(int player, int flag) {
-    smm_players.flag_graduated[player] = flag;
-}
-
-// 3. Get Function
-char* smmObj_getPlayerName(int player) {
-    return smm_players.player_name[player]; 
-}
-
-void smmObj_setPlayerName(int player, char* name) {
-    strcpy(smm_players.player_name[player], name);
-}
-
-int smmObj_getPlayerPos(int player) {
-    return smm_players.player_pos[player];
-}
-
-int smmObj_getPlayerCredit(int player) {
-    return smm_players.player_credit[player];
-}
-
-int smmObj_getPlayerEnergy(int player) {
-    return smm_players.player_energy[player];
-}
-
-int smmObj_getGraduatedFlag(int player) {
-    return smm_players.flag_graduated[player];
-}
-
-//object generation
-int smmObj_genNode(char* name, int type, int credit, int energy)
-{
-	if (smm_nodeNr >= MAX_NODENR) {
-		return -1;
-	}
-	
-	smmObj_board[smm_nodeNr] = (smmObj_object_t*)malloc(sizeof(smmObj_object_t));
-	if (smmObj_board[smm_nodeNr] ==NULL) {
-		return -1;
-	}
-	
-	strcpy(smmObj_board[smm_nodeNr]->smm_name, name);
-  smmObj_board[smm_nodeNr]->smm_type = type;
-  smmObj_board[smm_nodeNr]->smm_credit = credit;
-	smmObj_board[smm_nodeNr]->smm_energy = energy;
-	smmObj_board[smm_nodeNr]->smm_grade = 0; // initialization
-  
-  
-  smm_nodeNr++;
+// GenNode
+int smmObj_genNode(char* name, int type, int credit, int energy) {
+    if (smm_nodeNr >= MAX_NODENR) return -1;
     
-  return (smm_nodeNr);
-
-}
-
-
-
-//member retrieving
-char* smmObj_getNodeName(int node_nr)
-{
-	return(smmObj_board[node_nr]->smm_name);
-}
-
-int smmObj_getNodeType(int node_nr)
-{
-	return(smmObj_board[node_nr]->smm_type);
-}
-
-int smmObj_getNodeCredit(int node_nr)
-{
-	return(smmObj_board[node_nr]->smm_credit);
-}
-
-int smmObj_getNodeEnergy(int node_nr)
-{
-	return(smmObj_board[node_nr]->smm_energy);
-}
-
-
-//element to string
-char* smmObj_getTypeName(smmNode_e type)
-{
-    return smmNodeName[type];
-}
-
-char* smmObj_getGradeName(smmGrade_e grade)
-{
-    return smmGradeName[grade];
-}
-
-smmGrade_e smmObj_getRandomGrade(void)
-{
-    // 0 ~ 8 number random generate and return
-    int random_val = rand() % MAX_ACHIEVABLE_GRADE;
-    return (smmGrade_e)random_val;
-}
-
-void smmObj_addGradeToHistory(int player, char *lectureName, int credit, smmGrade_e grade)
-{
-    smmGradeNode_t *newNode = (smmGradeNode_t *)malloc(sizeof(smmGradeNode_t));
-    if (newNode == NULL) {
-        return; 
-    }
+    smmObj_board[smm_nodeNr] = (smmObj_t*)malloc(sizeof(smmObj_t));
+    if (smmObj_board[smm_nodeNr] == NULL) return -1;
     
-    strcpy(newNode->lecture_name, lectureName);
-    newNode->credit = credit;
-    newNode->grade = grade;
-    newNode->next = NULL;
-    
-    int player_idx = player;
-    newNode->next = smm_players.grade_history_head[player_idx];
-    smm_players.grade_history_head[player_idx] = newNode;
+    strcpy(smmObj_board[smm_nodeNr]->smm_name, name);
+    smmObj_board[smm_nodeNr]->smm_type = type;
+    smmObj_board[smm_nodeNr]->smm_credit = credit;
+    smmObj_board[smm_nodeNr]->smm_energy = energy;
+    smmObj_board[smm_nodeNr]->smm_grade = 0;
+    smmObj_board[smm_nodeNr]->next = NULL;
+
+    return (++smm_nodeNr);
 }
 
-// Search Lecture in History
-struct smmGradeNode* smmObj_findLectureGrade(int player, char *lectureName)
-{
-    int player_idx = player;
-    smmGradeNode_t *current = smm_players.grade_history_head[player_idx];
+char* smmObj_getNodeName(int node_nr) { return smmObj_board[node_nr]->smm_name; }
+int smmObj_getNodeType(int node_nr) { return smmObj_board[node_nr]->smm_type; }
+int smmObj_getNodeCredit(int node_nr) { return smmObj_board[node_nr]->smm_credit; }
+int smmObj_getNodeEnergy(int node_nr) { return smmObj_board[node_nr]->smm_energy; }
+
+// Grade
+char* smmObj_getTypeName(smmNode_e type) { return smmNodeName[type]; }
+char* smmObj_getGradeName(smmGrade_e grade) { return smmGradeName[grade]; }
+smmGrade_e smmObj_getRandomGrade(void) { return (smmGrade_e)(rand() % MAX_ACHIEVABLE_GRADE); }
+
+void smmObj_addGradeToHistory(int player, char *lectureName, int credit, smmGrade_e grade) {
+    smmObj_t *newNode = (smmObj_t *)malloc(sizeof(smmObj_t));
+    if (newNode == NULL) return;
     
+    strcpy(newNode->smm_name, lectureName);
+    newNode->smm_credit = credit;
+    newNode->smm_grade = (int)grade;
+    newNode->next = smm_players.grade_history_head[player];
+    smm_players.grade_history_head[player] = newNode;
+}
+
+smmObj_t* smmObj_findLectureGrade(int player, char *lectureName) {
+    smmObj_t *current = smm_players.grade_history_head[player];
     while (current != NULL) {
-        if (strcmp(current->lecture_name, lectureName) == 0) {
-            return current; // Find. Pointer Return
-        }
+        if (strcmp(current->smm_name, lectureName) == 0) return current;
         current = current->next;
     }
-    
-    return NULL; // 찾지 못함
+    return NULL;
 }
 
-// Get History
-struct smmGradeNode* smmObj_getGradeHistoryHead(int player) {
-    return smm_players.grade_history_head[player];
-}
+smmObj_t* smmObj_getGradeHistoryHead(int player) { return smm_players.grade_history_head[player]; }
+char* smmObj_getHistoryLectureName(smmObj_t* node) { return node->smm_name; }
+int smmObj_getHistoryCredit(smmObj_t* node) { return node->smm_credit; }
+smmGrade_e smmObj_getHistoryGrade(smmObj_t* node) { return (smmGrade_e)node->smm_grade; }
+smmObj_t* smmObj_getNextHistoryNode(smmObj_t* node) { return node->next; }
 
-// EXP STATE Getter/Setter 
-int smmObj_getExpFlag(int player) {
-    return smm_players.is_experimenting[player];
-}
-
-void smmObj_updateExpFlag(int player, int flag) {
-    smm_players.is_experimenting[player] = flag;
-}
-
-int smmObj_getExpValue(int player) {
-    return smm_players.exp_success_value[player];
-}
-
-void smmObj_updateExpValue(int player, int value) {
-    smm_players.exp_success_value[player] = value;
-}
-
-// History
-char* smmObj_getHistoryLectureName(struct smmGradeNode* node) {
-    return node->lecture_name;
-}
-
-int smmObj_getHistoryCredit(struct smmGradeNode* node) {
-    return node->credit;
-}
-
-smmGrade_e smmObj_getHistoryGrade(struct smmGradeNode* node) {
-    return node->grade;
-}
-
-struct smmGradeNode* smmObj_getNextHistoryNode(struct smmGradeNode* node) {
-    return node->next;
-}
+// EXP
+int smmObj_getExpFlag(int player) { return smm_players.is_experimenting[player]; }
+void smmObj_updateExpFlag(int player, int flag) { smm_players.is_experimenting[player] = flag; }
+int smmObj_getExpValue(int player) { return smm_players.exp_success_value[player]; }
+void smmObj_updateExpValue(int player, int value) { smm_players.exp_success_value[player] = value; }
--- basecode/smm_object.h
+++ basecode/smm_object.h
@@ -8,18 +8,21 @@
 #ifndef smm_object_h
 #define smm_object_h
 
-#define MAX_CHARNAME               			 200
+#define MAX_CHARNAME 200
 
+// Structure
+typedef struct smmObj {
+    char smm_name[MAX_CHARNAME];
+    int smm_type;
+    int smm_credit;
+    int smm_energy;
+    char smm_mission[MAX_CHARNAME * 3];
+    int smm_grade;
+    struct smmObj* next; // 연결 리스트용 포인터 [cite: 44]
+} smmObj_t;
 
-typedef struct {
-	char smm_name[MAX_CHARNAME];// array size [100][200]
-	int smm_type;
-	int smm_credit;
-	int smm_energy;
-	int smm_grade;
-} smmObj_object_t;
-
-struct smmGradeNode;
+// Type
+typedef smmObj_t smmGradeNode_t;
 
 typedef enum {
     SMMNODE_TYPE_LECTURE = 0,
@@ -31,75 +34,52 @@ typedef enum {
     SMMNODE_TYPE_FESTIVAL,
 } smmNode_e;
 
-
 typedef enum {
-	GRADE_A_PLUS = 0,
-	GRADE_A_ZERO ,
-	GRADE_A_MINUS,
-	GRADE_B_PLUS,
-	GRADE_B_ZERO,
-	GRADE_B_MINUS,
-	GRADE_C_PLUS,
-	GRADE_C_ZERO,
-	GRADE_C_MINUS,
-	GRADE_D_PLUS,
-	GRADE_D_ZERO,
-	GRADE_D_MINUS,
-	GRADE_F
-}smmGrade_e;
+    GRADE_A_PLUS = 0, GRADE_A_ZERO, GRADE_A_MINUS,
+    GRADE_B_PLUS, GRADE_B_ZERO, GRADE_B_MINUS,
+    GRADE_C_PLUS, GRADE_C_ZERO, GRADE_C_MINUS,
+    GRADE_D_PLUS, GRADE_D_ZERO, GRADE_D_MINUS, GRADE_F
+} smmGrade_e;
 
-
-
-//object generation
+// Prototype
 int smmObj_genNode(char* name, int type, int credit, int energy);
-
-// PlayerNr setting and return
 void smmObj_updatePlayerNr(int n);
 int smmObj_getPlayerNr(void);
-
-// Player Initialization
 void smmObj_initPlayerFields(int player, int initEnergy);
 
-//member retrieving
-char* smmObj_getNodeName(int node_nr); // return node name
-int smmObj_getNodeType(int node_nr); // return node type
-int smmObj_getNodeCredit(int node_nr); // return credit
-int smmObj_getNodeEnergy(int node_nr); // return energy
+char* smmObj_getNodeName(int node_nr);
+int smmObj_getNodeType(int node_nr);
+int smmObj_getNodeCredit(int node_nr);
+int smmObj_getNodeEnergy(int node_nr);
 
-// Player Update functions 
 void smmObj_updatePlayerPos(int player, int pos);
 void smmObj_updatePlayerCredit(int player, int credit);
 void smmObj_updatePlayerEnergy(int player, int energy);
 void smmObj_updateGraduatedFlag(int player, int flag);
 char* smmObj_getPlayerName(int player);
-void smmObj_updateExpFlag(int player, int flag);
-void smmObj_updateExpValue(int player, int value);
-
-// Player Set functions 
 void smmObj_setPlayerName(int player, char* name);
 int smmObj_getPlayerPos(int player);
 int smmObj_getPlayerCredit(int player);
 int smmObj_getPlayerEnergy(int player);
 int smmObj_getGraduatedFlag(int player);
+
+char* smmObj_getTypeName(smmNode_e type);
+char* smmObj_getGradeName(smmGrade_e grade);
+smmGrade_e smmObj_getRandomGrade(void);
+
+// Exp
+void smmObj_updateExpFlag(int player, int flag);
 int smmObj_getExpFlag(int player);
+void smmObj_updateExpValue(int player, int value);
 int smmObj_getExpValue(int player);
 
-
-//element to string
-char* smmObj_getTypeName(smmNode_e type); // return node type name(char*)
-char* smmObj_getGradeName(smmGrade_e grade);
-
-smmGrade_e smmObj_getRandomGrade(void);
+// Grade
 void smmObj_addGradeToHistory(int player, char *lectureName, int credit, smmGrade_e grade);
-struct smmGradeNode* smmObj_findLectureGrade(int player, char *lectureName);
-struct smmGradeNode* smmObj_getGradeHistoryHead(int player);
-
-// History prototype
-char* smmObj_getHistoryLectureName(struct smmGradeNode* node);
-int smmObj_getHistoryCredit(struct smmGradeNode* node);
-smmGrade_e smmObj_getHistoryGrade(struct smmGradeNode* node);
-struct smmGradeNode* smmObj_getNextHistoryNode(struct smmGradeNode* node);
-
-struct smmGradeNode* smmObj_findLectureGrade(int player, char *lectureName);
+smmObj_t* smmObj_findLectureGrade(int player, char *lectureName);
+smmObj_t* smmObj_getGradeHistoryHead(int player);
+char* smmObj_getHistoryLectureName(smmObj_t* node);
+int smmObj_getHistoryCredit(smmObj_t* node);
+smmGrade_e smmObj_getHistoryGrade(smmObj_t* node);
+smmObj_t* smmObj_getNextHistoryNode(smmObj_t* node);
 
 #endif/* smm_object_h */
